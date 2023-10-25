cae6e3c1cbb190ff6ed35697d76cf5a6aa9bbb6a
Luckysunday-git <luckysunday001@gmail.com>
2023-10-25 08:51:13 -0700

    fixing menu icon for mobile devices

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector('.menu-btn');
+
+const navLinks = document.querySelector('.ul-navlinks');
+
+menuBtn.addEventListener('click', () =>{
+  navLinks.classList.toggle('mobile-menu');
+});
+
+   
+    
+
+
+
