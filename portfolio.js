75db192f4b3e698a81547816f4a099f70db15757
Luckysunday-git <luckysunday001@gmail.com>
2023-10-26 08:43:05 -0700

    adding fixed navbar onscroll

--- portfolio.js
+++ portfolio.js
@@ -4,10 +4,35 @@ const navLinks = document.querySelector('.ul-navlinks');
 
 menuBtn.addEventListener('click', () =>{
   navLinks.classList.toggle('mobile-menu');
+  
 });
 
    
-    
+    // for fixed navbar onscroll
+
+   let height = 0;
+
+   let scrollHeight = () => { 
+    return height + scrollY;
+   }
+
+  window.addEventListener('scroll', () =>{
+  const scroll = Math.floor(scrollHeight());
+  console.log(scroll);
+  
+  let navHeader = document.querySelector('.nav-header');
+
+  if(!scroll){
+  navHeader.style.backgroundColor = 'transparent';
+  navHeader.style.borderBottom = 'transparent';
+  }else if(scroll){
+    navHeader.style.backgroundColor = '#5f7d95';
+    navHeader.style.borderBottom = `${1}px solid black`;
+  }
+
+})
+
+
 
 
 
