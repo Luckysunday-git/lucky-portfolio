8d0c19de54f0f83b97796928888819ee7d631464
Luckysunday-git <luckysunday001@gmail.com>
2023-11-01 12:56:05 -0700

    adding dark-theme toggle button and animation on about-container

--- portfolio.js
+++ portfolio.js
@@ -1,36 +1,106 @@
+// fixing the mobile screen menu icon onclick.
 const menuBtn = document.querySelector('.menu-btn');
-
 const navLinks = document.querySelector('.ul-navlinks');
-
 menuBtn.addEventListener('click', () =>{
   navLinks.classList.toggle('mobile-menu');
-  
+
 });
 
-   
-    // for fixed navbar onscroll
+// updating the footer alright reserved date.
 
-   let height = 0;
+const footerDate = document.querySelector('.date');
+const currentDate = new Date().getFullYear();
+footerDate.innerHTML = currentDate;
 
-   let scrollHeight = () => { 
-    return height + scrollY;
-   }
+// for fixed navbar onscroll
+
+let height = 0;
+  let scrollHeight = () => { 
+   return height + scrollY;
+}
 
   window.addEventListener('scroll', () =>{
   const scroll = Math.floor(scrollHeight());
   console.log(scroll);
-  
-  let navHeader = document.querySelector('.nav-header');
 
+  let navHeader = document.querySelector('.nav-header');
+  
   if(!scroll){
   navHeader.style.backgroundColor = 'transparent';
   navHeader.style.borderBottom = 'transparent';
   }else if(scroll){
-    navHeader.style.backgroundColor = '#5f7d95';
-    navHeader.style.borderBottom = `${1}px solid black`;
+  navHeader.style.backgroundColor = '#5f7d95';
+  navHeader.style.borderBottom = `${2}px solid black`;
+}
+ 
+ const animateGoal = document.querySelector('.about-message-goal');
+ const animateLang = document.querySelector('.about-message-lang');
+ const animateEdu = document.querySelector('.about-message-edu');
+ 
+ if( scroll >= 250 && scroll <= 900){
+  //  animateGoal.style.backgroundColor = `rgb(${95, 125, 149})`;
+  //  animateLang.style.backgroundColor = 'white';
+  //  animateEdu.style.backgroundColor = 'white';s
+   animateGoal.style.transform = `translateX(${scroll * 0 }px)`
+   animateLang.style.transform = `translateX(${scroll * 0})`
+   animateEdu.style.transform = `translateY(${scroll * 0})`
+ }
+  
+})
+
+//when any of the nav link is clicked on mobile screen, the nav links disappear.
+
+const mobileScreens = document.querySelectorAll('.mobile-screen');
+mobileScreens.forEach((mobileScreen) => {
+  mobileScreen.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu');
+  })
+
+})
+  
+  const aboutContent = document.querySelector('.content');
+  const aboutContainer = document.querySelector('.about-container');
+  const listItems = document.querySelector('.list-items');
+  const numberExperience = document.querySelector('.number-experience');
+  const nav_Links = document.querySelector('.ul-navlinks');
+  const experienceContainer = document.querySelector('.experience-frontend-container');
+  const darkThemeContainer = document.querySelector('.dark-theme-container');
+  const lightMode = document.querySelector('.light-mode');
+
+   lightMode.addEventListener('click', () => {
+     if( lightMode.innerHTML === 'Light Mode'){
+    lightMode.innerHTML = 'Dark Mode';
+    document.body.style.backgroundColor = 'black';
+    lightMode.style.transform = `translate(${20}px)`
+    aboutContent.style.backgroundColor = 'black';
+    aboutContainer.style.backgroundColor = 'black';
+    experienceContainer.style.backgroundColor = 'black';
+    listItems.style.backgroundColor = 'black';
+    numberExperience.style.backgroundColor = 'black';
+    
+  }else if(lightMode.innerHTML === 'Dark Mode'){
+    lightMode.innerHTML = 'Light Mode';
+    document.body.style.backgroundColor = 'wheat';
+    lightMode.style.transform = `translate(-${20}px)`
+    aboutContent.style.backgroundColor = 'initial';
+    aboutContainer.style.backgroundColor = '#5f7d95';
+    experienceContainer.style.backgroundColor = 'white';
+    listItems.style.backgroundColor = '#5f7d95';
+    numberExperience.style.backgroundColor = '#5f7d95';
+    
+    
+  }else{
+    lightMode.innerHTML = 'Light Mode';
+    document.body.style.backgroundColor = 'wheat';
+    lightMode.style.transform = `translate(-${20}px)`
+    aboutContent.style.backgroundColor = 'initial'; 
+    aboutContainer.style.backgroundColor = '#5f7d95';
+    
   }
 
 })
+
+
 
 
 
